c45df0ad4ed31dbc220843ed0865b365d35a189f
WalterChen352 <111105449+WalterChen352@users.noreply.github.com>
2025-02-05 15:22:09 -0800

    additional add and remove features for database

--- GoNOW/app/index.tsx
+++ GoNOW/app/index.tsx
@@ -1,11 +1,12 @@
 import TabNavigator from './screens/Navigator';
 import { useEffect } from 'react';
-import {initializeDatabase, getDailyEvents} from './scripts/database';
+import {initializeDatabase, getDailyEvents, clearEvents} from './scripts/database';
 
 export default function Index() {
   useEffect(() => {
     initializeDatabase();
-    getDailyEvents();
+    //getDailyEvents();
+    //clearEvents();
   }, []);
   return (
     <TabNavigator></TabNavigator>
--- GoNOW/app/scripts/database.js
+++ GoNOW/app/scripts/database.js
@@ -14,9 +14,9 @@ export const initializeDatabase = async () => {
          latitude REAL,
          longitude REAL,
          transportationMode TEXT);
-         INSERT INTO events (name, description, startTime, endTime, latitude, longitude, transportationMode) VALUES ('Study1', 'Descr1', '2025-02-05 16:00:00', '2025-02-05 16:30:00', 0, 0, 'walking');
-         INSERT INTO events (name, description, startTime, endTime, latitude, longitude, transportationMode) VALUES ('Study2', 'Descr2', '2025-02-05 12:00:00', '2025-02-05 12:30:00', 0, 0, 'walking');
-         INSERT INTO events (name, description, startTime, endTime, latitude, longitude, transportationMode) VALUES ('Study3', 'Descr3', '2025-03-05 16:00:00', '2025-03-05 16:30:00', 0, 0, 'walking');
+        --INSERT INTO events (name, description, startTime, endTime, latitude, longitude, transportationMode) VALUES ('Study1', 'Descr1', '2025-02-05 16:00:00', '2025-02-05 16:30:00', 0, 0, 'walking');
+         --INSERT INTO events (name, description, startTime, endTime, latitude, longitude, transportationMode) VALUES ('Study2', 'Descr2', '2025-02-05 12:00:00', '2025-02-05 12:30:00', 0, 0, 'walking');
+         --INSERT INTO events (name, description, startTime, endTime, latitude, longitude, transportationMode) VALUES ('Study3', 'Descr3', '2025-03-05 16:00:00', '2025-03-05 16:30:00', 0, 0, 'walking');
     `);
         
 
@@ -36,11 +36,26 @@ export const getDailyEvents = async()=>{
         const DB = await SQLite.openDatabaseAsync('userEvents.db');
         const readOnly=true;
         console.log('db opened for daily events');
-        const result = await DB.getAllAsync("SELECT * FROM events");
+        const result = await DB.getAllAsync("SELECT * FROM events WHERE startTime BETWEEN date('now') AND date('now', '+1 day')");
         console.log(result);
         console.log('done txn');
+        return result;
     }
     catch{
         console.error('error getting daily events', error);
     }
+}
+
+export const clearEvents = async()=>{
+  console.log('dropping events table');
+  try{
+      const DB = await SQLite.openDatabaseSync('userEvents.db');
+      await DB.execAsync(`PRAGMA journal_mode = WAL;
+        DROP TABLE events;
+        `)
+      console.log('dropped table')
+  }
+  catch{
+    console.error(' error dropping table', error);
+  }
 }
