7c0b958f27bc2f6d8fb81d1a8ca107d4ac0a1804
Miri Liu <emmatliu@gmail.com>
2025-02-05 17:21:15 -0800

    basic display of events implemented.  TODO: workflow integration, click to open tasks, change date

--- GoNOW/app/screens/DailyScreen.tsx
+++ GoNOW/app/screens/DailyScreen.tsx
@@ -1,8 +1,69 @@
-import { Text } from "react-native"
+import React, { useState, useEffect } from 'react';
+import { Text, View, FlatList } from 'react-native';
+import { getDailyEvents } from "../scripts/database";
+import { styles } from './screen_styles/DailyScreen.styles';
 
+interface Event {
+  id: number;
+  name: string;
+  description: string;
+  startTime: string;
+  endTime: string;
+  latitude: number;
+  longitude: number;
+  transportationMode: string;
+}
 
-const DailyScreen = ()=>{
-    return (<Text>Daily Screen</Text>);
+const DailyScreen = () => {
+  // set up
+  const [events, setEvents] = useState<Event[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const loadEvents = async () => {
+    try {
+        setLoading(true);
+        const dailyEvents = await getDailyEvents();
+        setEvents(dailyEvents);
+    } catch (error) {
+        console.error('Failed to get daily events.', error);
+    } finally {
+        setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    loadEvents();
+  }, []);
+
+  const displayEvent = ({ item }: { item: Event }) => (
+    <View style={styles.eventCard}>
+      <Text style={styles.eventTitle}>{item.name}</Text>
+      <Text style={styles.eventDescription}>{item.description}</Text>
+      <Text style={styles.eventTime}>
+        Time: {new Date(item.startTime).toLocaleTimeString()} - {new Date(item.endTime).toLocaleTimeString()}
+      </Text>
+    </View>
+  );
+
+  if (loading) {
+    return <Text style={styles.loading}>
+        Loading events!
+    </Text>
+  }
+
+  return (
+    <View style={styles.container}>
+      {events.length > 0 ? (
+        <FlatList<Event>
+          data={events}
+          renderItem={displayEvent}
+          keyExtractor={item => item.id.toString()}
+        />
+      ) : (
+        <Text style={styles.noEvents}>No tasks today!</Text>
+      )}
+    </View>
+  );
 };
 
 export default DailyScreen;
