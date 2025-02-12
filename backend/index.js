c03131549832ee191c4946c4e16d1a053484b8c9
Miri Liu <emmatliu@gmail.com>
2025-02-11 19:59:01 -0800

    final fixes -- minor linting issue that I didn't catch because I didn't lint backend, and also having npm run lint do --fix automatically so we don't have to fix errors that are minor.

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,11 @@ const PORT = 3000;
 
 require('dotenv').config();
 
-const apiKey= process.env.API_KEY;
+// eslint-disable-next-line no-unused-vars
+const apiKey= process.env.API_KEY; 
+// TODO: Adding a lint ignore to make it go through for now, but
+// we should eventually remove this disable when we have implemented
+// more features with api key.
 
 app.get('/api/autoschedule', (req, res)=>{
     //TODO
@@ -13,7 +17,7 @@ app.get('/api/autoschedule', (req, res)=>{
     //determine some candidates
     //send send back to user
     res.send('received autoschedule request');
-})
+});
 
 app.get('/api/poll', (req, res)=>{
     //TODO
@@ -21,8 +25,8 @@ app.get('/api/poll', (req, res)=>{
     //query against google maps api to ensure arrival time
     //send updated travel time back to user
     res.send('received poll request');
-})
+});
 
 app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}`)
-  })
+    console.log(`Example app listening on port ${PORT}`);
+  });
