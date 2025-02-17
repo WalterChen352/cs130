82752031d0bfdbc64e4cd5ec9c6dac069864022e
WalterChen352 <111105449+WalterChen352@users.noreply.github.com>
2025-02-17 15:17:37 -0800

    one more fix

--- GoNOW/app/index.tsx
+++ GoNOW/app/index.tsx
@@ -13,6 +13,8 @@ useEffect(() => {
           await addEvent('Study1', 'Descr1', '2025-02-05 16:00:00', '2025-02-05 16:30:00', 0, 0, 'walking');
           await addEvent('Study2', 'Descr2', '2025-02-05 12:00:00', '2025-02-05 12:30:00', 0, 0, 'walking');
           await addEvent('Study3', 'Descr3', '2025-03-05 16:00:00', '2025-03-05 16:30:00', 0, 0, 'walking');
+          // console.log("Events added.");
+
           const events = await getDailyEvents(); // Fetch events after adding
           console.log('Daily events:', events);
           await clearEvents(); // Clear events after fetching
