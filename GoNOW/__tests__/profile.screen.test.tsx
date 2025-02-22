acf4355dff329a5458cc4cfc230fcd17c0f7b82f
Anna Kokoulina <annakoko@ucla.edu>
2025-02-22 15:08:09 -0800

    Fix profile screen tests

--- GoNOW/__tests__/profile.screen.test.tsx
+++ GoNOW/__tests__/profile.screen.test.tsx
@@ -2,11 +2,10 @@ import React from 'react';
 import { act, render, fireEvent, waitFor } from '@testing-library/react-native';
 import { useNavigation } from '@react-navigation/native';
 
-//Commented Out to appease linter while test gets fixed
-//import { getWorkflows} from '../app/scripts/Workflow';
-//import { Workflow } from '../app/models/Workflow';
-//import { Time } from '../app/models/Time';
-// import { SchedulingStyle } from '../app/models/SchedulingStyle';
+import { getWorkflows} from '../app/scripts/Workflow';
+import { Workflow } from '../app/models/Workflow';
+import { Time } from '../app/models/Time';
+import { SchedulingStyle } from '../app/models/SchedulingStyle';
 
 import ProfileScreen from '../app/screens/ProfileScreen';
 import { View } from 'react-native';
@@ -52,35 +51,34 @@ jest.mock('../app/scripts/Profile', () => {
   };
 });
 
-//Commented out to appease linter while test gets fixed
-// const mockSchedulingStyles = [
-//   new SchedulingStyle(0, 'Schedule close together'),
-//   new SchedulingStyle(1, 'Schedule with max buffer'),
-//   new SchedulingStyle(2, 'Schedule with mmiddle buffer'),
-//   new SchedulingStyle(3, 'Schedule with random buffer')
-// ];r
-// const mockWorkflows = [
-//   new Workflow(
-//     1,
-//     'School',
-//     '#d5f9cf',
-//     false,
-//     new Time(9, 0),
-//     new Time(10, 0),
-//     [false, true, true, false, true, false, false],
-//     mockSchedulingStyles[0]
-//   ),
-//   new Workflow(
-//     2,
-//     'Errand',
-//     '#d3eef9',
-//     true,
-//     new Time(11, 0),
-//     new Time(17, 0),
-//     [true, false, false, false, false, false, true],
-//     mockSchedulingStyles[1]
-//   ),
-// ];
+const mockSchedulingStyles = [
+  new SchedulingStyle(0, 'Schedule close together'),
+  new SchedulingStyle(1, 'Schedule with max buffer'),
+  new SchedulingStyle(2, 'Schedule with mmiddle buffer'),
+  new SchedulingStyle(3, 'Schedule with random buffer')
+];
+const mockWorkflows = [
+  new Workflow(
+    1,
+    'School',
+    '#d5f9cf',
+    false,
+    new Time(9, 0),
+    new Time(10, 0),
+    [false, true, true, false, true, false, false],
+    mockSchedulingStyles[0]
+  ),
+  new Workflow(
+    2,
+    'Errand',
+    '#d3eef9',
+    true,
+    new Time(11, 0),
+    new Time(17, 0),
+    [true, false, false, false, false, false, true],
+    mockSchedulingStyles[1]
+  ),
+];
 
 describe('Profile Screen', () => {
   const mockNavigation = { navigate: jest.fn() };
@@ -98,33 +96,35 @@ describe('Profile Screen', () => {
       expect(getByTestId('home-location-title')).toHaveTextContent('Home Location');
     });
   });
-  //**Test was not actually being called when originally pushed to main.**
-  // test('should load and render workloads list on profile screen', async () => {
-  //   (getWorkflows as jest.Mock).mockResolvedValue(mockWorkflows);
-  //   const { getByTestId } = render(<ProfileScreen />);
+  test('should load and render workloads list on profile screen', async () => {
+    (getWorkflows as jest.Mock).mockResolvedValue(mockWorkflows);
+    const { findByTestId } = render(<ProfileScreen />);
 
-  //   for (const workflow of mockWorkflows) {
-  //     await waitFor(() => {
-  //       const workflowElement = getByTestId(`workflow-${workflow.id}`);
-  //       const headerElement = getByTestId(`workflow-name`);
-  //       const textElement = getByTestId(`workflow-time-start`);
-  //       const styleElement = getByTestId(`workflow-scheduling-style`);
+    await waitFor(async () => {
+      for (const w of mockWorkflows) {
+        const workflowElement = await findByTestId(`workflow-${String(w.id)}`);
+        const headerElement = await findByTestId(`workflow-header-${String(w.id)}`);
+        const textElement = await findByTestId(`workflow-text-${String(w.id)}`);
+        const timeStartElement = await findByTestId(`workflow-text-${String(w.id)}`);
+        const timeEndElement = await findByTestId(`workflow-text-${String(w.id)}`);
+        const styleElement = await findByTestId(`workflow-scheduling-style-${String(w.id)}`);
 
-  //       expect(workflowElement).toBeTruthy();
-  //       expect(headerElement).toHaveTextContent(new RegExp(workflow.name, 'i'));
-  //       expect(textElement).toHaveTextContent(/Schedule/);
-  //       expect(textElement).toHaveTextContent(new RegExp(workflow.timeStart.toString(), 'i'));
-  //       expect(textElement).toHaveTextContent(new RegExp(workflow.timeEnd.toString(), 'i'));
-  //       expect(styleElement).toHaveTextContent(new RegExp(workflow.schedulingStyle.Name, 'i'));
-  //     });
-  //   }
-  // });
+        expect(workflowElement).toBeTruthy();
+        expect(headerElement).toHaveTextContent(new RegExp(w.name, 'i'));
+        expect(textElement).toHaveTextContent(/Schedule/);
+        expect(timeStartElement).toHaveTextContent(new RegExp(w.timeStart.toString(), 'i'));
+        expect(timeEndElement).toHaveTextContent(new RegExp(w.timeEnd.toString(), 'i'));
+        expect(styleElement).toHaveTextContent(new RegExp(w.schedulingStyle.Name, 'i'));
+      }
+    });
+  });
 
-  test('should navigate to Workflow create screen when press Create Workload button', () => {
+  test('should navigate to Workflow create screen when press Create Workload button', async () => {
     const { getByTestId } = render(<ProfileScreen />);
     
-    act(() => {
+    await act(async () => {
       fireEvent.press(getByTestId('workflow-btn-add'));
+      return Promise.resolve();
     });
 
     expect(mockNavigation.navigate).toHaveBeenCalledWith('Workflow', expect.any(Object));
