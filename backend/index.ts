ff342904bc9c89ba5afce9e47673d21dec5612c3
WalterChen352 <111105449+WalterChen352@users.noreply.github.com>
2025-02-22 16:05:29 -0800

    implemented route querying and added some polylines.

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,24 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
-
+import type { Headers} from 'node-fetch';
 dotenv.config();
 
 const app: Express = express();
 const PORT = 3000;
-const apiKey = process.env.API_KEY;  // eslint-disable-line
+const bodyParser= require('body-parser');
+app.use(bodyParser.json())
+
+const apiKey = process.env.API_KEY;   
+
+
+
+interface RouteRequestBody {
+    oriLat: number;
+    oriLong: number;
+    destLat: number;
+    destLong: number;
+    travelMode: string;
+  }
 
 // TODO: Adding a lint ignore to make it go through for now, but
 // we should eventually remove this disable when we have implemented
@@ -21,10 +34,54 @@ app.get('/api/autoschedule', (req: Request, res: Response) => {
 
 app.get('/api/poll', (req: Request, res: Response) => {
     //TODO
+    //parse incoming parameters
+    //take into account existing events
+    //determine some candidates
+    //send send back to user
+    res.send('received autoschedule request');
+});
+
+app.get('/api/route', async (req: Request<unknown, unknown, RouteRequestBody>, res: Response) => {
+    //TODO
     //parse incoming task for location
-    //query against google maps api to ensure arrival time
-    //send updated travel time back to user
-    res.send('received poll request');
+    console.log(req.body)
+    const { oriLat, oriLong, destLat, destLong, travelMode } = req.body;
+    
+    //construct request
+    const url = 'https://routes.googleapis.com/directions/v2:computeRoutes';
+
+    const headers = new Headers({
+        'Content-Type': 'application/json',
+        'X-Goog-Api-Key': apiKey ?? '',
+        'X-Goog-FieldMask': 'routes.polyline.encodedPolyline'
+    });
+
+    const body = JSON.stringify({
+        origin: {
+            location: { latLng: { latitude: oriLat, longitude: oriLong } }
+        },
+        destination: {
+            location: { latLng: { latitude: destLat, longitude: destLong } }
+        },
+        travelMode: travelMode
+    });
+
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: headers,
+            body: body
+        });
+
+        if (!response.ok) throw new Error(`HTTP Error: ${String(response.status)}`);
+
+        const json = await response.json() as Record<string, unknown>;
+        res.send(json);
+
+    } catch (error) {
+        console.error('Error fetching route:', error);
+        res.status(500).send('');
+    }
 });
 
 app.listen(PORT, () => {
