e25f339814eb0c5923c9992e7e46c12993bda2f9
Anna-Koko <157985516+Anna-Koko@users.noreply.github.com>
2025-02-24 18:40:50 -0800

    Update transportation.scripts.test.ts

--- GoNOW/__tests__/transportation.scripts.test.ts
+++ GoNOW/__tests__/transportation.scripts.test.ts
@@ -11,19 +11,19 @@ describe('Transportation Mode Scripts', () => {
 
     expect(getTransportationMode(1).id).toEqual(1);
     expect(getTransportationMode(1).name).toEqual('Walk');
-    expect(getTransportationMode(1).google).toEqual('walking');
+    expect(getTransportationMode(1).apiName).toEqual('walking');
 
     expect(getTransportationMode(2).id).toEqual(2);
     expect(getTransportationMode(2).name).toEqual('Bus');
-    expect(getTransportationMode(2).google).toEqual('transit');
+    expect(getTransportationMode(2).apiName).toEqual('transit');
 
     expect(getTransportationMode(3).id).toEqual(3);
     expect(getTransportationMode(3).name).toEqual('Bike');
-    expect(getTransportationMode(3).google).toEqual('bicycling');
+    expect(getTransportationMode(3).apiName).toEqual('bicycling');
 
     expect(getTransportationMode(4).id).toEqual(4);
     expect(getTransportationMode(4).name).toEqual('Car');
-    expect(getTransportationMode(4).google).toEqual('driving');
+    expect(getTransportationMode(4).apiName).toEqual('driving');
   });
 
   test('getTransportationMode should return default transportation mode when id is not found', () => {
