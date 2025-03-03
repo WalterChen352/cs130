c4f24368524177cad7381bdd6bed395b335d375e
Anna Kokoulina <annakoko@ucla.edu>
2025-03-02 19:01:00 -0800

    Update AddressPicker to select place on map.

--- GoNOW/__tests__/address.picker.test.tsx
+++ GoNOW/__tests__/address.picker.test.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import { render, fireEvent, waitFor } from '@testing-library/react-native';
+import { act, render, fireEvent, waitFor } from '@testing-library/react-native';
 import AddressPicker from '../app/components/AddressPicker';
 import { Coordinates, Location } from '../app/models/Location';
+import { IoniconsProps } from '../__mocks__/ionicons'
 
 jest.mock('lodash/debounce', () => {
   return jest.fn((fn: (...args: unknown[]) => void) => fn);
 });
+
+jest.mock('react-native-vector-icons/Ionicons', () => {
+  return function MockIonicons(props: IoniconsProps) {
+    return <mock-ionicon {...props} />;
+  };
+});
+
+jest.mock('../app/scripts/Geo', () => ({
+  getAddressByCoordinates: jest.fn(() => Promise.resolve('Kerckhoff Hall')),
+}));
 
 global.fetch = jest.fn(() =>
   Promise.resolve({
@@ -60,6 +71,37 @@ describe('AddressPicker', () => {
     expect(onSelectMock).toHaveBeenCalledWith(
       expect.objectContaining(new Location(new Coordinates(34.0689604, -118.4449446), '7400 Boelter Hall, Los Angeles, CA 90095'))
     );
+  });
+
+  test('should handles click on place selection from search results and opens the map', async () => {
+    const onSelectMock = jest.fn();
+    const { getByTestId } = render(
+      <AddressPicker onSelect={onSelectMock} />
+    );
+    act(() => { fireEvent.press(getByTestId('map-btn')); });
+    await waitFor(() => { expect(getByTestId('map-btn-save')).toBeTruthy() });
+  });
+
+  test('shold handle dradNdrop marker on map and save its coordinates', async () => {
+    const onSelectMock = jest.fn();
+    const initialCoordinates = new Coordinates(34.0522, -118.2437);
+    const { getByTestId } = render(
+      <AddressPicker initialCoordinates={initialCoordinates} onSelect={onSelectMock} />
+    );
+    fireEvent.press(getByTestId('map-btn'));
+    const mockEvent = {
+      nativeEvent: { coordinate: { latitude: 34.0689604, longitude: -118.4463165 } },
+    };
+    // Drag marker
+    act(() => { fireEvent(getByTestId('map-marker'), 'onDragEnd', mockEvent); });
+    await waitFor(() => { expect(getByTestId('map-btn-save')).toBeTruthy(); });
+    // Click Save
+    act(() => { fireEvent.press(getByTestId('map-btn-save')); });
+    await waitFor(() => {
+      expect(onSelectMock).toHaveBeenCalledWith(
+        new Location(new Coordinates(34.0689604, -118.4463165), 'Kerckhoff Hall')
+      );
+    });
   });
 
 });
