497e8bf1fc8e6c889e57973933c5ab7304726b9a
WalterChen352 <111105449+WalterChen352@users.noreply.github.com>
2025-03-06 14:44:21 -0800

    deploy script

--- backend/test.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-:: This batch file runs a PowerShell command to get user info from Google API
-
-:: Run the PowerShell command
-powershell -Command "$token = gcloud auth print-identity-token; Invoke-WebRequest -Uri 'https://www.googleapis.com/oauth2/v1/userinfo' -Headers @{ Authorization = 'Bearer $token' } -UseBasicParsing"
-
-:: Pause to see the output (optional)
-pause
