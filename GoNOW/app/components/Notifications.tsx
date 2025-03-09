4a1fd9c77e3289e60987f7bd876dab2c285028d0
Miri Liu <emmatliu@gmail.com>
2025-03-09 16:03:46 -0700

    Revert "fixes to make it work for me"
    This reverts commit d45cd0ce9611e58eb39f675cf904206a8c99f54e.

--- GoNOW/app/components/Notifications.tsx
+++ GoNOW/app/components/Notifications.tsx
@@ -1,20 +1,35 @@
-import React, { useState, useEffect, useRef } from 'react';
-import * as Notifications from 'expo-notifications';
-import { S_PER_MIN } from '../scripts/Polling';
+import { useState, useEffect, useRef } from "react";
+// import { Platform } from "react-native";
+import * as Notifications from "expo-notifications";
+import { S_PER_MIN } from "../scripts/Polling";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({  //eslint-disable-line
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 const NotificationDisplay: React.FC = () => {
-  const [notification, setNotification] = useState<Notifications.Notification>();
+  const [notification, setNotification] = useState<Notifications.Notification | undefined>(undefined);  //eslint-disable-line
+  const [channels, setChannels] = useState<Notifications.NotificationChannel[]>([]);  //eslint-disable-line
+
   const notificationListener = useRef<Notifications.EventSubscription>();
   const responseListener = useRef<Notifications.EventSubscription>();
 
   useEffect(() => {
-    notificationListener.current = Notifications.addNotificationReceivedListener((notif) => {
-      console.log('Notification received:', notif);
-      setNotification(notif);
+    // requestNotificationPermission();
+    // if (Platform.OS === 'android') {
+    //   Notifications.getNotificationChannelsAsync().then(value => { setChannels(value ?? []); });
+    // }
+
+    notificationListener.current = Notifications.addNotificationReceivedListener((notification) => {
+      setNotification(notification);
     });
 
     responseListener.current = Notifications.addNotificationResponseReceivedListener((response) => {
-      console.log('User responded to notification:', response);
+      console.log(response);
     });
 
     return () => {
@@ -27,23 +42,28 @@ const NotificationDisplay: React.FC = () => {
     };
   }, []);
 
+  // async function requestNotificationPermission() {
+  //   const { status } = await Notifications.requestPermissionsAsync();
+  //   if (status !== "granted") {
+  //     alert("Permission to receive notifications is required!");
+  //   }
+  // }
+  
   return null;
-};
+}
 
 export async function scheduleLocalNotification(title: string, body: string, min_in_future: number) {
-  console.log(`Scheduling notification: ${title}, ${body}, in ${min_in_future} minutes`);
   await Notifications.scheduleNotificationAsync({
     content: {
       title: title,
       body: body,
-      data: { customData: 'some data' },
+      data: { customData: "some data" },
     },
     trigger: {
       seconds: min_in_future * S_PER_MIN,
       channelId: 'GoNOW Notifications',
     },
   });
-  console.log('Notification scheduled');
 }
 
 export default NotificationDisplay;
